f97e2dfbe7918eac37cb300c30202950954692b8
Ammar Ahmed <ammarahmed6506@gmail.com>
2025-02-12 18:04:11 +0500

    fix perf 1

--- platforms/android/test-app/runtime/src/main/cpp/runtime/callbackhandlers/CallbackHandlers.h
+++ platforms/android/test-app/runtime/src/main/cpp/runtime/callbackhandlers/CallbackHandlers.h
@@ -63,7 +63,7 @@ namespace tns {
         static napi_value
         CallJavaMethod(napi_env env, napi_value caller, const std::string &className,
                        const std::string &methodName, MetadataEntry *entry, bool isFromInterface,
-                       bool isStatic, bool isSuper, napi_callback_info info,  size_t argc, napi_value* argv);
+                       bool isStatic, napi_callback_info info,  size_t argc, napi_value* argv);
 
         static napi_value
         CallJSMethod(napi_env env, JNIEnv *jEnv, napi_value jsObject,jclass claz,
