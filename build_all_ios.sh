603a5018129f035b36e20ddd8c9a7a613242cbea
DjDeveloperr <hey@djdev.me>
2025-06-11 13:15:19 -0400

    fix: pass engine flag through to build_nativescript.sh

--- build_all_ios.sh
+++ build_all_ios.sh
@@ -3,11 +3,12 @@ set -e
 
 rm -rf ./dist
 # don't run if NO_UPDATE_VERSION is set
-if [ -z "$NO_UPDATE_VERSION" ]; then
-  ./update_version.sh
-fi
+# if [ -z "$NO_UPDATE_VERSION" ]; then
+  # TODO: integrate version into runtime
+  # ./update_version.sh
+# fi
 ./build_metadata_generator.sh
-./build_nativescript.sh --no-vision
+./build_nativescript.sh --no-vision $1
 ./build_tklivesync.sh --no-vision
 ./prepare_dSYMs.sh
 ./build_npm_ios.sh
