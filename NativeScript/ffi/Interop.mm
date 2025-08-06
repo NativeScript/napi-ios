3349ca968cd8cf6d8cd409041668d362772c61cb
DjDeveloperr <hey@djdev.me>
2025-08-06 14:09:41 -0400

    allocate reference default type

--- NativeScript/ffi/Interop.mm
+++ NativeScript/ffi/Interop.mm
@@ -810,7 +810,7 @@ napi_value Reference::constructor(napi_env env, napi_callback_info info) {
       reference->type->toNative(env, argv[1], reference->data, &shouldFree, &shouldFree);
     }
   } else if (argc == 0) {
-    reference->data = nullptr;
+    reference->data = malloc(sizeof(void*));
     const char * typestr = "@";
     reference->type = TypeConv::Make(env, &typestr);
     reference->initValue = nullptr;
