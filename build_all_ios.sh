c4d8d4b11cdf80076be7ec5bea6c760ecd5a977b
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-08-20 10:17:47 +0900

    avoid running build_npm_ios.sh when --no-engine flag is passed

--- build_all_ios.sh
+++ build_all_ios.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+TARGET_ENGINE=${TARGET_ENGINE:=v8} # default to v8 for compat
+
+# See build_nativescript.sh for all supported flags. This parent script is only
+# interested in --no-engine.
+for arg in $@; do
+  case $arg in
+    --v8) TARGET_ENGINE=v8 ;;
+    --quickjs) TARGET_ENGINE=quickjs ;;
+    --jsc) TARGET_ENGINE=jsc ;;
+    --hermes) TARGET_ENGINE=hermes ;;
+    --no-engine) TARGET_ENGINE=none ;;
+    *) ;;
+  esac
+done
+
 rm -rf ./dist
 # don't run if NO_UPDATE_VERSION is set
 # if [ -z "$NO_UPDATE_VERSION" ]; then
@@ -11,4 +26,14 @@ rm -rf ./dist
 ./build_nativescript.sh --no-vision $1
 ./build_tklivesync.sh --no-vision
 ./prepare_dSYMs.sh
-./build_npm_ios.sh
+
+if [[ "$TARGET_ENGINE" == "none" ]]; then
+  # If you're building *with* --no-engine, you're trying to make an npm release
+  # of a workspace under ./packages/*, like @nativescript/ios-node-api.
+  echo "Skipping build_npm_ios.sh due to --no-engine flag."
+  echo "build_all_ios.sh finished!"
+else
+  # If you're building *without* --no-engine, you're trying to make an npm
+  # release of the root-level workspace, @nativescript/ios.
+  ./build_npm_ios.sh
+fi
--- build_all_vision.sh
+++ build_all_vision.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 set -e
 
+TARGET_ENGINE=${TARGET_ENGINE:=v8} # default to v8 for compat
+
+# See build_nativescript.sh for all supported flags. This parent script is only
+# interested in --no-engine.
+for arg in $@; do
+  case $arg in
+    --v8) TARGET_ENGINE=v8 ;;
+    --quickjs) TARGET_ENGINE=quickjs ;;
+    --jsc) TARGET_ENGINE=jsc ;;
+    --hermes) TARGET_ENGINE=hermes ;;
+    --no-engine) TARGET_ENGINE=none ;;
+    *) ;;
+  esac
+done
+
 rm -rf ./dist
 ./update_version.sh
 ./build_metadata_generator.sh
-./build_nativescript.sh --no-catalyst --no-iphone --no-sim
+./build_nativescript.sh --no-catalyst --no-iphone --no-sim $1
 ./build_tklivesync.sh --no-catalyst --no-iphone --no-sim
 ./prepare_dSYMs.sh
-./build_npm_vision.sh
+
+if [[ "$TARGET_ENGINE" == "none" ]]; then
+  # If you're building *with* --no-engine, you're trying to make an npm release
+  # of a workspace under ./packages/*, like @nativescript/ios-node-api.
+  echo "Skipping build_npm_vision.sh due to --no-engine flag."
+  echo "build_all_vision.sh finished!"
+else
+  # If you're building *without* --no-engine, you're trying to make an npm
+  # release of the root-level workspace, @nativescript/ios.
+  ./build_npm_vision.sh
+fi
