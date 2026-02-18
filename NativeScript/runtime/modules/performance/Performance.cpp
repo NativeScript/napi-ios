c14b50cbb9699be04fc38cc4519561db5fedde4c
DjDeveloperr <hey@djdev.me>
2026-02-18 15:53:48 -0500

    feat: addd timeOrigin to performance object

--- NativeScript/runtime/modules/performance/Performance.cpp
+++ NativeScript/runtime/modules/performance/Performance.cpp
@@ -3,11 +3,29 @@
 #include "js_native_api.h"
 #include "mach/mach_time.h"
 #include "native_api_util.h"
+#include <chrono>
 
 namespace nativescript {
 
+namespace {
+double g_timeOrigin = 0;
+
+double monotonicNowMs() {
+  uint64_t time = mach_absolute_time();
+  mach_timebase_info_data_t timebase;
+  mach_timebase_info(&timebase);
+  double nanoseconds =
+      (double)time * (double)timebase.numer / (double)timebase.denom;
+  return nanoseconds / 1000000.0;
+}
+}  // namespace
+
 JS_CLASS_INIT(Performance::Init) {
   napi_value Performance, performance;
+  const auto systemNow = std::chrono::duration_cast<std::chrono::milliseconds>(
+                             std::chrono::system_clock::now().time_since_epoch())
+                             .count();
+  g_timeOrigin = (double)systemNow - monotonicNowMs();
 
   const napi_property_descriptor properties[] = {
       {
@@ -20,10 +38,24 @@ JS_CLASS_INIT(Performance::Init) {
           .attributes = napi_default,
           .data = nullptr,
       },
+      {
+          .utf8name = "timeOrigin",
+          .name = nullptr,
+          .method = nullptr,
+          .getter = [](napi_env env, napi_callback_info info) -> napi_value {
+            napi_value result;
+            napi_create_double(env, g_timeOrigin, &result);
+            return result;
+          },
+          .setter = nullptr,
+          .value = nullptr,
+          .attributes = napi_default,
+          .data = nullptr,
+      },
   };
 
   napi_define_class(env, "Performance", NAPI_AUTO_LENGTH,
-                    Performance::Constructor, nullptr, 1, properties,
+                    Performance::Constructor, nullptr, 2, properties,
                     &Performance);
 
   napi_new_instance(env, Performance, 0, nullptr, &performance);
@@ -52,15 +84,8 @@ JS_METHOD(Performance::Constructor) {
 }
 
 JS_METHOD(Performance::Now) {
-  uint64_t time = mach_absolute_time();
-  mach_timebase_info_data_t timebase;
-  mach_timebase_info(&timebase);
-  double nanoseconds =
-      (double)time * (double)timebase.numer / (double)timebase.denom;
-  double milliseconds = nanoseconds / 1000000.0;
-
   napi_value result;
-  napi_create_double(env, milliseconds, &result);
+  napi_create_double(env, monotonicNowMs(), &result);
   return result;
 }
 
