a488912ee1f89398682ef80ac21da1f4327eeba4
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-02-20 20:31:59 +0900

    npx create-react-native-library@latest

---
+++ packages/jsi/README.md
@@ -0,0 +1,37 @@
+# nativescript-jsi
+
+A JSI projection for NativeScript
+
+## Installation
+
+
+```sh
+npm install nativescript-jsi
+```
+
+
+## Usage
+
+
+```js
+import { multiply } from 'nativescript-jsi';
+
+// ...
+
+const result = multiply(3, 7);
+```
+
+
+## Contributing
+
+- [Development workflow](CONTRIBUTING.md#development-workflow)
+- [Sending a pull request](CONTRIBUTING.md#sending-a-pull-request)
+- [Code of conduct](CODE_OF_CONDUCT.md)
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ packages/jsi/android/src/main/java/com/nativescriptjsi/NativescriptJsiPackage.kt
@@ -0,0 +1,31 @@
+package com.nativescriptjsi
+
+import com.facebook.react.BaseReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
+import java.util.HashMap
+
+class NativescriptJsiPackage : BaseReactPackage() {
+  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+    return if (name == NativescriptJsiModule.NAME) {
+      NativescriptJsiModule(reactContext)
+    } else {
+      null
+    }
+  }
+
+  override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
+    mapOf(
+      NativescriptJsiModule.NAME to ReactModuleInfo(
+        name = NativescriptJsiModule.NAME,
+        className = NativescriptJsiModule.NAME,
+        canOverrideExistingModule = false,
+        needsEagerInit = false,
+        isCxxModule = false,
+        isTurboModule = true
+      )
+    )
+  }
+}
